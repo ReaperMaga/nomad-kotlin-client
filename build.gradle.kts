08b0847c30a07b9a4ce6a1724e8c1a3e0dcee8c5
ReaperMaga <reapermaga@gmail.com>
2025-12-14 19:35:40 +0100

    feat: enhance build configuration with Dokka and publishing setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
+
 plugins {
     kotlin("jvm") version "2.3.0-RC"
     kotlin("plugin.serialization") version "2.3.0-RC"
     id("io.kotest") version "6.0.7"
+    id("org.jetbrains.dokka") version "2.1.0"
+    `maven-publish`
 }
 
 group = "dev.reapermaga"
-version = "0.1.0"
+version = extraProperties["global.version"]!! as String
 
 repositories {
     mavenCentral()
@@ -25,4 +29,45 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+
+java {
+    withSourcesJar()
+}
+
+dokka { }
+
+tasks.register<Jar>("dokkaHtmlJar") {
+    dependsOn(tasks.dokkaGenerateHtml)
+    from(layout.buildDirectory.dir("dokka/html"))
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            // Use the default Maven artifact coordinates (group, name, version).
+            from(components["java"])
+            artifact(tasks["dokkaHtmlJar"])
+
+            groupId = project.group.toString()
+            artifactId = project.name
+
+            version = findProperty("build.version")?.toString()
+                ?: System.getenv("BUILD_VERSION")
+                        ?: extraProperties["global.version"]!! as String
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Averix"
+            url = uri("https://repo.averix.tech/repository/maven-releases/")
+            credentials {
+                username = project.findProperty("averix.user")?.toString() ?: System.getenv("AVERIX_USER")
+                password = project.findProperty("averix.key")?.toString() ?: System.getenv("AVERIX_KEY")
+            }
+        }
+    }
 }
